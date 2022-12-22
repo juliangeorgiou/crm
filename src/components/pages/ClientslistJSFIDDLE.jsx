011dae102aab42cb6a84a3e0c11049d0400a12df
Julian Georgiou <georgiou.julian@gmail.com>
2022-12-22 12:00:15 +0100

    Filtering by nationalities Showing all nationalities on startup

--- src/components/pages/ClientslistJSFIDDLE.jsx
+++ src/components/pages/ClientslistJSFIDDLE.jsx
@@ -44,10 +44,13 @@ export default function ClientslistJSFIDDLE() {
   ]
 
   const nationalityOptions = clients
+  //map clients by nationality
     .map((client) => client.clientNationality)
+    //filter those results by keeping only the first occurence of each item
     .filter((element, index, array) => {
       return array.indexOf(element) === index
     })
+    //map those results to the dropdown with value and label as per the library specs(react-select)
     .map((nationality) => {
       return {value: nationality, label: nationality}
   })
@@ -56,14 +59,22 @@ export default function ClientslistJSFIDDLE() {
   const [isEditingTable, setIsEditingTable] = useState(false)
   const [ascending, setAscending] = useState(true)
   const [filterMenuShown, setFilterMenuShown] = useState(false)
+  const [nationalitiesShown, setNationalitiesShown] = useState([])
   const [columnIsShown, setColumnIsShown] = useState({
     clientID: true,
     clientName: true,
-    orderValue: false,
+    orderValue: true,
     clientNationality: true
   })
-  //sort clients ascending or descending
-  clients.sort((a, b) => {
+
+  //filter clients
+  const filteredClients = clients.filter((client) => {
+    //SHORT CIRCUITING: first true occurence for || stops and first false occurence for && stops
+    return nationalitiesShown.length === 0 || nationalitiesShown.includes(client.clientNationality)
+  })
+
+  //sort filtered clients ascending or descending
+  filteredClients.sort((a, b) => {
     const valueA = a[columnSort]
     const valueB = b[columnSort]
     if (typeof valueA === "string") {
@@ -144,16 +155,21 @@ export default function ClientslistJSFIDDLE() {
           borderRadius: "2px"
         }}>
           <MultiRangeSlider />
-          <Select options={nationalityOptions} />
+          <Select 
+          options={nationalityOptions} 
+          isMulti 
+          onChange={(newValue) => setNationalitiesShown(newValue.map( (nationalityObject) => nationalityObject.value))}/>
         </div>
         : ""}
       </div>
       <Table striped bordered hover>
         <thead>
-          <tr style={isEditingTable ? {
-            border: "1px solid blue",
-            borderStyle: "double"
-          } : {}}>
+          <tr style={isEditingTable 
+            ? {
+              border: "1px solid blue",
+              borderStyle: "double"
+            } 
+            : {}}>
 
             {renderColumnHeader("clientID", "ID")}
             {renderColumnHeader("clientName", "Name")}
@@ -162,7 +178,7 @@ export default function ClientslistJSFIDDLE() {
           </tr>
         </thead>
         <tbody>
-          {clients.map((client) => (
+          {filteredClients.map((client) => (
             <tr key={client.clientID}>
               {renderColumnValue("clientID", client)}
               {renderColumnValue("clientName", client)}
@@ -174,7 +190,7 @@ export default function ClientslistJSFIDDLE() {
           <tr>
           <td>Total</td>
           <td></td>
-          <td>{clients.reduce((sum, client) => {
+          <td>{filteredClients.reduce((sum, client) => {
           	return sum + client.orderValue
            }, 0)}</td>
         </tr>
